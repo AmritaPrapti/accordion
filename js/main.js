eabfe15f1edb6f0067b31ff0d5593c95c93d92fc
Amrita <amrita@gmail.com>
2021-10-05 15:04:09 +0600

    submit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const accordionItemHeader = document.querySelectorAll(".accordion__header");
+
+accordionItemHeader.forEach(accordionItemHeader => {
+
+    accordionItemHeader.addEventListener("click", event=> {
+
+        accordionItemHeader.classList.toggle("active");
+
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+
+            if(accordionItemHeader.classList.contains("active")) {
+                accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+            }
+            else {
+                accordionItemBody.style.maxHeight = 0;
+            }
+    });
+});
